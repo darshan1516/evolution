041b483f8c3dec9e56cbc890fbcc687f58abbf17
darshan1516 <47447861+darshan1516@users.noreply.github.com>
2019-02-08 19:32:05 +0530

    Create alaphabet.c

---
+++ alaphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	char c;
+	if(c=='a'||c=='b'||c=='c'||c=='d'||c=='e'||c=='f'||c=='e'||c=='g'||c=='h'||c=='i'||c=='j'||c=='k'||c=='l'||c=='m'||c=='n'||c=='o'||c=='p'||c=='q'||c=='r'||c=='s'||c=='t'||c=='u'||c=='v'||c=='w'||c=='x'||c=='y'||c=='z')
+	{
+		printf("it is alphabet");
+		}
+		else{
+		printf("it is not alphabet");
+		}
+
+}
